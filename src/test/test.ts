80e789ebb874d613c9a704fac387e20ad69f306a
“chongqw  git config user.name “chongqw <chongqw@xports.cn>
2021-10-13 16:12:06 +0800

    test some ts

--- src/test/test.ts
+++ src/test/test.ts
@@ -6,3 +6,22 @@ export default function logginTd<T extends LenghtWise>(args:T):T{
   console.log(args.length)
   return args
 }
+
+function loggingIdentity<T>(arg: Array<T>): Array<T> {
+  console.log(arg.length); // Array has a .length, so no more error
+  return arg;
+}
+// 隐式
+loggingIdentity(['asd'])
+// 显式
+loggingIdentity<Number>([1])
+
+interface TestConstructor<T> {
+  new(): T
+}
+
+function create<T>(c: TestConstructor<T>): T {
+  return new c();
+}
+create(Number)
+create<String>(String)
